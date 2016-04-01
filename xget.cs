7b341b6dce71d9e4f294583f1d958ccf003c92d2
thewatcher <thewatcheriswatchinu@gmail.com>
2016-04-01 20:59:19 +0200

    initial commit

---
+++ xget.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System;
+using System.Collections.Generic;
+
+class wClient
+{
+	public static void Main(string[] args)
+	{
+		String[] arguments = Environment.GetCommandLineArgs();
+		WebClient webClient = new WebClient();
+		webClient.DownloadFile(arguments[1], arguments[2]);
+		Console.WriteLine ("File to download:");
+		Console.WriteLine(arguments[1]);
+		Console.WriteLine ("Destination for file:");
+		Console.WriteLine (arguments [2]);
+
+	}
+}
