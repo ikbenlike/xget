7076364067a7a4fdb70a63648860bf5791288325
thewatcher <thewatcheriswatchinu@gmail.com>
2016-04-02 18:16:52 +0200

    now automatically extracts a filename from the url when no destination is provided

--- xget.cs
+++ xget.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class wClient
 {
@@ -8,14 +9,26 @@ class wClient
 	{
 		String[] arguments = Environment.GetCommandLineArgs();
 		WebClient webClient = new WebClient();
+
 		try 
 		{
 			Console.WriteLine ("File to download:\n" + arguments [1] + "\nDestination:\n" + arguments [2]);
-			webClient.DownloadFile(arguments[1], arguments[2]);
+			webClient.DownloadFile(arguments[1], arguments[2]);	
 		}
+
 		catch (IndexOutOfRangeException) {
-			Console.WriteLine("please input a valid URL and destination");
-			throw;
+			try
+			{
+				char[] splitChars = { '/' };
+				string linkToDownload = arguments[1];
+				string[] fileDest = linkToDownload.Split(splitChars);
+				Console.WriteLine("File to download:\n" + arguments[1] + "\nDestination:\n" + fileDest.Last());
+				webClient.DownloadFile(arguments[1], fileDest.Last());
+			}
+			catch (IndexOutOfRangeException) {
+				Console.WriteLine ("please input a valid URL and destination");
+				throw;
+			}
 		}
 	}
 }
